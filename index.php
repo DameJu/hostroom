b81a35fd2137532c3c4957bab47177e4cc454a57
Juliette <jliiette.meuniier@gmail.com>
2021-02-17 12:06:02 +0100

    ajout
    j'ai commencé la base

--- index.php
+++ index.php
@@ -5,8 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/hostroom.css">
 </head>
 <body>
-    <p>coucou</p>
+    <header>
+        <nav>
+            <ul class="menu">
+                <li class="menu2"><a href="#">Accueil</a></li>
+                <li class="menu2"><a href="#">Chambres</a></li>
+                <li class="menu2"><a href="#">Lien office de tourisme</a></li>
+                <li class="menu2"><a href="#">Page de réservation</a></li>
+                <li class="menu2"><a href="#">Contact</a></li>
+                <li class="menu2"><a href="#">Commentaires</a></li>
+                <li class="menu2"><a href="#">Plan d'accès</a></li>
+                
+            </ul>
+        </nav>
+
+        <div class="parallax">
+            
+        </div>
+    </header>
+
+    
+
+    <footer>
+        <p>Contact, adresse</p>
+    </footer>
 </body>
 </html>
