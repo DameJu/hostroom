8d89f3f8538db03a6d2b8683a41a8846560e917a
Juliette <jliiette.meuniier@gmail.com>
2021-02-24 11:09:06 +0100

    ajout du sous-menu "chambres"
    html, css

--- index.php
+++ index.php
@@ -13,21 +13,24 @@
             <ul class="menu">
                 <li class="menu2"><a href="#">Accueil</a></li>
                 <li class="menu2"><a href="#">Chambres</a></li>
+                    <ul class ="under">
+                        <li><a href ="#">Chambre 1</a></li>
+                        <li><a href ="#">Chambre 2</a></li>
+                        <li><a href ="#">Chambre 3</a></li>
+                    </ul>
                 <li class="menu2"><a href="#">Lien office de tourisme</a></li>
                 <li class="menu2"><a href="#">Page de réservation</a></li>
                 <li class="menu2"><a href="#">Contact</a></li>
                 <li class="menu2"><a href="#">Commentaires</a></li>
                 <li class="menu2"><a href="#">Plan d'accès</a></li>
-                
+                <button id="connexion">Connexion</button>
             </ul>
         </nav>
 
-        <div class="parallax">
-            
-        </div>
+        <div class="parallax"></div>
     </header>
 
-    
+    <section>Bondour</section>
 
     <footer>
         <p>Contact, adresse</p>
