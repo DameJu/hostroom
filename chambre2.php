1816ec19828b9d00bfccbd06fefc7b471750f705
daniel <danbigot@sfr.fr>
2021-02-26 15:11:23 +0100

    changement

---
+++ chambre2.php
@@ -0,0 +1,42 @@
+<body>
+    <section class="section2">
+        <section class="section_border">
+            <h3>Chambre Premier Étage (Coquillages)</h3>
+            <p id="texte2">Chambre au premier étage de notre maison.<br>
+                           Avec un lit de deux personnes, salle d'eau et WC privé à la chambre.<br>
+                           Prix :<br>
+                           1 personne 60 €<br>
+                           2 personnes 80 €<br>
+                           Taxe de séjour 0,70 Cts / Personne / Jour</p>
+        </section>
+
+    </section>
+
+    <h5>Photos de la chambre  Premier Étage (Coquillages) </h5>
+    <section class="premieretage">
+        <section id="galerie">
+            <div class="grid_container">
+                <div class="box grid_item">
+                    <img src="./images/image.jpg">
+
+                </div>
+
+                <div class="box grid_item">
+                    <img src="./images/chambreetage.jpg">
+
+                </div>
+
+                <div class="box grid_item">
+                    <img src="./images/chambreetage1.jpg">
+
+                </div>
+                <div class="box grid_item">
+                    <img src="./images/chambreetage2.jpg">
+
+                </div>
+            </div>
+        </section>
+
+
+
+</body>
---
+++ sejour.php
@@ -0,0 +1,39 @@
+<body>
+    <section class="section4">
+        <section class="section_border">
+            <h3>
+                La salle du petit déjeuner
+            </h3>
+            <p id="texte4">r</p>
+        </section>
+
+    </section>
+
+    <h5>Photos de la chambre  Premier Étage (Coquillages) </h5>
+    <section class="premieretage">
+        <section id="galerie">
+            <div class="grid_container">
+                <div class="box grid_item">
+                    <img src="./images/chambre2etage.jpg">
+
+                </div>
+
+                <div class="box grid_item">
+                    <img src="./images/chambre2etage1.jpg">
+
+                </div>
+
+                <div class="box grid_item">
+                    <img src="./images/chambre2etage2.jpg">
+
+                </div>
+                <div class="box grid_item">
+                    <img src="./images/chambre2etage3.jpg">
+
+                </div>
+            </div>
+        </section>
+
+
+
+</body>
