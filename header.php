cfbdda77652b7b7e4b5a27d7a3f747286a62ed98
Juliette <jliiette.meuniier@gmail.com>
2021-02-26 15:08:16 +0100

    html, css

---
+++ header.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+   
+   <link rel="stylesheet" href="./css/header.css">
+
+</head>
+<body>
+<header>
+        
+        <div id="menu">
+            <ul>
+                <li class="menu2"><a href="#">Accueil</a></li>
+                <li class="menu2"><a href="#">Chambres</a>
+                    <ul>
+                        <li><a href="#">Chambre 1</a></li>
+                        <li><a href="#">Chambre 2</a></li>
+                        <li><a href="#">Chambre 3</a></li>
+                    </ul>
+                </li>
+                <li class="menu2"><a href="#">Lien office de tourisme</a></li>
+                <li class="menu2"><a href="#">Page de réservation</a></li>
+                <li class="menu2"><a href="#">Contact</a></li>
+                <li class="menu2"><a href="#">Commentaires</a></li>
+                <li class="menu2"><a href="#">Plan d'accès</a></li>
+                
+            </ul>
+        </div>
+        <button id="connexion">Connexion</button>
+        
+    </header>
+
+
+
---
+++ parallax.php
@@ -0,0 +1,14 @@
+<?php
+include './header.php';
+?>
+
+<section>
+
+<div class="parallax"></div>
+
+
+</section>
+
+<?php
+include './footer.php';
+?>
