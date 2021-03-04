dce3ac4db25daa184b92bdd3e3539aee20ea22a3
Juliette <jliiette.meuniier@gmail.com>
2021-03-04 15:22:12 +0100

    lien offices de tourismes

--- office_de_tourisme.php
+++ office_de_tourisme.php
@@ -2,6 +2,15 @@
 include './header.php';
 ?>
 
+Les Offices de tourisme alentours:
+<br>
+<a href="https://www.berck-tourisme.com/fr/"> Berck </a> <br>
+<a href="http://www.stella-plage.fr/">stella</a> <br>
+<a href="https://www.tourisme-etaples.com/fr/accueil/">étaples</a> <br>
+<a href="https://www.letouquet.com/">touquet</a> <br>
+
+
+
 
 <?php
 include './footer.php';
