de81fb030a74aa0daf5326cc71e7849a75f74bd7
daniel <danbigot@sfr.fr>
2021-03-05 10:05:31 +0100

    changement du 05

--- chambre1.php
+++ chambre1.php
@@ -13,7 +13,7 @@ include './header.php';
         <h1>Chambre Rez-de-Chaussée</h1>
 
     </fieldset>
-        
+
         <p id="texte1"> Une chambre double, deux lits d'une personne.<br>
                         Micro-onde et réfrigérateur, salle d'eau et WC privé à la chambre.<br>
                         Prix :<br>
@@ -28,13 +28,13 @@ include './header.php';
     <div class="gallery">
             <div class="image_sejour">
                 
-                <img class="photos" src="./images/image (2).jpg">
+                <img class="photos" src="./images/IMG_0483.JPG">
 
-                <img class="photos" src="./images/IMG_1653.JPG">
+                <img class="photos" src="./images/IMG_0480.JPG">
                 
-                <img class="photos" src="./images/chamres côté jardin 011.JPG">
+                <img class="photos" src="./images/IMG_0481.JPG">
 
-                <img class="photos" src="./images/IMG_1655.JPG">
+                <img class="photos" src="./images/IMG_0482.JPG">
 
                 
             </div>
