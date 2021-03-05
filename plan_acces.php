f727b4404e3848b854808d06cfa8010c534a2068
daniel <danbigot@sfr.fr>
2021-03-05 11:56:34 +0100

    chang 11.56

--- plan_acces.php
+++ plan_acces.php
@@ -2,6 +2,15 @@
 include './header.php';
 ?>
 
+<h3 class="footer_contact"> ATTENTION ! Deux AVENUE DES PEUPLIERS  une sur STELLA notre plage et une sur TRÉPIED.
+
+Coordonnées GPS :  N 50.49449° / E 1.62036°
+
+Ou dans vos GPS AVENUE LOUCHEUR,  au giratoire 2ème à droite AVENUE DU BOIS et première à gauche  AVENUE DES PEUPLIERS. 
+
+</h3> 
+
+<a href="https://www.google.fr/maps/@50.4946863,1.6200176,274m/data=!3m1!1e3"> Itinéraire pour arriver chez nous</a>
 
 <?php
 include './footer.php';
