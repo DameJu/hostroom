5bb0fa71a106797c941ca0431dd4cb62615c1b22
Juliette <jliiette.meuniier@gmail.com>
2021-03-05 10:05:54 +0100

    footer

--- footer.php
+++ footer.php
@@ -3,31 +3,25 @@
 
 <fieldset id="cadre">
 
-    <div class="cb">
-        <img src="./images/cb.jpg">
-    </div>
+<div class="cb"><img src="./images/cb.jpg"></div>
     
-    <div class="cb">
-
-<p class="footer_contact">  Catherine et Daniel BIGOT  <br>
-
-Vous accueillent dans leur maison : <br>
-47, Avenue des Peupliers <br>
-Parc Soleil <br>
-62780 CUCQ TREPIED STELLA PLAGE <br> <br>
-Tél : 03.21.09.38.41 ou 
-Portable : 06.21.46.68.98 <br>
-E-mail : bigot.catherine@club-internet.fr <br> <br>
-Coordonnées GPS : <br>
-N 50.49449° <br>
-E 1.62036° <br> 
-Animaux non admis </h3></strong>
-</div>
-
 <div class="cb">
+    <h3 class="footer_contact">  Catherine et Daniel BIGOT  <br>
 
-     <img src="./images//ancv.jpg" alt="">
+    Vous accueillent dans leur maison : <br>
+    47, Avenue des Peupliers <br>
+    Parc Soleil <br>
+    62780 CUCQ TREPIED STELLA PLAGE <br> <br>
+    Tél : 03.21.09.38.41 ou 
+    Portable : 06.21.46.68.98 <br>
+    E-mail : bigot.catherine@club-internet.fr <br> <br>
+    Coordonnées GPS : <br>
+    N 50.49449° <br>
+    E 1.62036° <br> 
+    Animaux non admis </h3>
 </div>
+
+<div class="cb"><img src="./images//ancv.jpg" alt=""></div>
 
 
 </fieldset>
