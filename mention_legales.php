8adc3d613dad476e5ff5e32e09cee836ecd44680
daniel <danbigot@sfr.fr>
2021-03-05 14:35:47 +0100

    chang14.35

---
+++ mention_legales.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<p>
+
+Mentions légales<br>
+La poursuite de la navigation sur ce site vaut acceptation sans réserve des dispositions et conditions d’utilisation qui suivent.<br>
+La version actuellement en ligne de ces conditions d’utilisation est la seule opposable pendant toute la durée d’utilisation du site et jusqu’à 
+ce qu’une nouvelle version la remplace.<br>
+
+Article 1 – Informations légales<br>
+1.1 – Site : http://www.welcome.fr<br>
+1.2 – Éditeur : Welcome, chambres d'hôtes dont le siège social est situé à Cucq au 47 avenue des peupliers Parc Soleil Trépied 62780,
+ numéro SIRET xxx xxx xxx xxx, représentée par Catherine et Daniel Bigot, en leur qualité de fondateurs.<br>
+Numéro de téléphone : 06 21 46 68 98<br>
+Adresse de courrier électronique : bigot.catherine@club-internet.fr<br>
+Directeur de la publication : Catherine Bigot<br>
+1.3 – Conception et réalisation : Dame Juliette et Lieutenant Dan Développeur d'applications Web et Mobile.<br>
+1.4 – Hébergeur : OVH<br>
+
+Article 2 – Accès au site
+L’accès au site est ouvert aux personnes majeures et mineures.<br> 
+L’éditeur se réserve le droit de demander toute justification de votre âge notamment si vous utilisez des services de commande en ligne.<br>
+L’accès au site et son utilisation sont réservés à un usage strictement personnel.<br>
+Vous vous engagez à ne pas utiliser ce site et les informations ou données qui y figurent à des fins commerciales, politiques,
+publicitaires et pour toute forme de sollicitation commerciale et notamment l’envoi de courriers électroniques non sollicités.<br>
+
+Article 3 – Contenu du site<br>
+Toutes les marques, photographies, textes, commentaires, illustrations, images animées ou non, séquences vidéo, 
+sons, ainsi que toutes les applications informatiques qui pourraient être utilisées pour faire fonctionner 
+ce site et plus généralement tous les éléments reproduits ou utilisés sur le site sont protégés par les lois en vigueur 
+au titre de la propriété intellectuelle.<br>
+Ils sont la propriété pleine et entière de l’éditeur ou de ses partenaires. Toute reproduction, représentation, 
+utilisation ou adaptation, sous quelque forme que ce soit, de tout ou partie de ces éléments, y compris les applications informatiques, 
+sans l’accord préalable et écrit de l’éditeur, sont strictement interdites. Le fait pour l’éditeur de ne pas engager de procédure dès 
+la prise de connaissance de ces utilisations non autorisées ne vaut pas acceptation desdites utilisations et renonciation aux poursuites.<br>
+
+Article 4 – Gestion du site<br>
+Pour la bonne gestion du site, l’éditeur pourra à tout moment :<br>
+-suspendre, interrompre ou de limiter l’accès à tout ou partie du site, réserver l’accès au site, ou à certaines parties du site, 
+à une catégorie déterminée d’internaute ;<br>
+-supprimer toute information pouvant en perturber le fonctionnement ou entrant en contravention avec les lois nationales ou internationales, 
+ou avec les règles de la Nétiquette ;<br>
+-suspendre le site afin de procéder à des mises à jour.<br>
+
+Article 5 – Responsabilités<br>
+La responsabilité de l’éditeur ne peut être engagée en cas de défaillance, panne, difficulté ou interruption de fonctionnement, 
+empêchant l’accès au site ou à une de ses fonctionnalités.<br>
+Le matériel de connexion au site que vous utilisez est sous votre entière responsabilité.<br> 
+Vous devez prendre toutes les mesures appropriées pour protéger votre matériel et vos propres données notamment d’attaques virales par Internet. 
+Vous êtes par ailleurs le seul responsable des sites et données que vous consultez.<br>
+L’éditeur ne pourra être tenu responsable en cas de poursuites judiciaires à votre encontre :<br>
+-du fait de l’usage du site ou de tout service accessible via Internet ;<br>
+-du fait du non-respect par vous des présentes conditions générales.<br>
+L’éditeur n’est pas responsable des dommages causés à vous-même, à des tiers et/ou à votre équipement du fait de votre connexion ou 
+de votre utilisation du site et vous renoncez à toute action contre lui de ce fait.<br>
+Si l’éditeur venait à faire l’objet d’une procédure amiable ou judiciaire à raison de votre utilisation du site, 
+il pourra retourner contre vous pour obtenir indemnisation de tous les préjudices, sommes, 
+condamnations et frais qui pourraient découler de cette procédure.<br>
+
+Article 6 – Liens hypertextes<br>
+La mise en place par vous de tous liens hypertextes vers tout ou partie du site est strictement interdite, sauf autorisation préalable et
+ écrite de l’éditeur, sollicitée par courriel à l’adresse suivante : generationinspire@hotmail.com<br>
+L’éditeur est libre de refuser cette autorisation sans avoir à justifier de quelque manière que ce soit sa décision.<br> 
+Dans le cas où l’éditeur accorderait son autorisation, celle-ci n’est dans tous les cas que temporaire et pourra être retirée à tout moment, 
+sans obligation de justification à la charge de l’éditeur.Dans tous les cas, tout lien devra être retiré sur simple demande de l’éditeur.<br>
+Toute information accessible via un lien vers d’autres sites n’est pas sous le contrôle de l’éditeur qui décline toute responsabilité 
+quant à leur contenu.<br>
+
+Article 7 – Protection des données à caractère personnel<br>
+Les informations recueillies sur le formulaire de contact sont enregistrées dans un fichier informatisé par Génération Inspire pour usage interne. 
+La base légale du traitement est l’intérêt légitime.<br>
+Les données collectées seront communiquées aux seuls destinataires suivants : Génération Inspire et ses partenaires d’action directs.
+Les données sont conservées pendant 5 ans.<br>
+Vous pouvez accéder aux données vous concernant, les rectifier, demander leur effacement ou exercer votre droit à la limitation du traitement de vos données.
+Consultez le site cnil.fr pour plus d’informations sur vos droits.<br>
+Si vous estimez, après nous avoir contactés, que vos droits « Informatique et Libertés » ne sont pas respectés, 
+vous pouvez adresser une réclamation à la CNIL.<br>
+
+Article 8 – Cookies<br>
+Le site peut collecter automatiquement des informations standards. Toutes les informations collectées indirectement ne seront utilisées que 
+pour suivre le volume, le type et la configuration du trafic utilisant ce site, pour en développer la conception et l’agencement et 
+à d’autres fins administratives et de planification et plus généralement pour améliorer le service que nous vous offrons.<br>
+
+Article 9 – Photographies et représentation des produits<br>
+Les photographies de produits, accompagnant leur description, ne sont pas contractuelles et n’engagent pas l’éditeur.<br>
+
+Article 10 – Loi applicable<br>
+Les présentes conditions d’utilisation du site sont régies par la loi française et soumises à la compétence des tribunaux de Saint-Etienne, 
+sous réserve d’une attribution de compétence spécifique découlant d’un texte de loi ou réglementaire particulier.<br>
+
+Article 11 – Contactez-nous<br>
+Pour toute question, information sur les produits présentés sur le site, ou concernant le site lui-même, 
+vous pouvez laisser un message à l’adresse suivante : contact@generation-inspire.com<br>
+</p>
