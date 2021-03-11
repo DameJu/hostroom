15eae16f7edca3e36bd9b97e9d0600b15347e927
Juliette <jliiette.meuniier@gmail.com>
2021-03-11 15:10:35 +0100

    img ancv

--- plan_acces.php
+++ plan_acces.php
@@ -7,10 +7,9 @@ include './header.php';
 <br>
 Ou dans vos GPS "AVENUE LOUCHEUR", <br>  au giratoire prendre la 2ème à droite "AVENUE DU BOIS" <br> et la première à gauche  "AVENUE DES PEUPLIERS". 
 </h3> 
+<br>
 
-<a href="https://www.google.fr/maps/place/Welcom'Opale/@50.4767472,1.5958185,18z/data=!4m13!1m7!3m6!1s0x47ddda4364d63fc1:0x6593cea7d0916a29!2sAvenue+des+Peupliers,+62780+Cucq!3b1!8m2!3d50.4770894!4d1.5958303!3m4!1s0x47ddda437d8124b3:0xe0520df4fe730250!8m2!3d50.4767946!4d1.5956435"> <img id="plan" src="./images/plan.jpg" alt=""></a>
 
-<a href="https://www.google.fr/maps/@50.4946863,1.6200176,274m/data=!3m1!1e3"> Itinéraire pour arriver chez nous</a>
 
 <?php
 include './footer.php';
