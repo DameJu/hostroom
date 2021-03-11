ccbfe4c6e6dfe05421b8c057300f695bc728630b
daniel <danbigot@sfr.fr>
2021-03-11 15:11:09 +0100

    chang 15.11

--- plan_acces.php
+++ plan_acces.php
@@ -5,12 +5,13 @@ include './header.php';
 <h3 class="texte" class="footer_contact"> ATTENTION ! <br> Il existe deux AVENUE DES PEUPLIERS : une sur STELLA (notre plage) et une sur TRÉPIED.
 <br>
 <br>
-Ou dans vos GPS "AVENUE LOUCHEUR", <br>  au giratoire prendre la 2ème à droite "AVENUE DU BOIS" <br> et la première à gauche  "AVENUE DES PEUPLIERS". 
-</h3> 
+Ou dans vos GPS "AVENUE LOUCHEUR", <br>  au giratoire prendre la 2ème à droite "AVENUE DU BOIS" <br> 
+et la première à gauche  "AVENUE DES PEUPLIERS<br>
+</h3>
+<h3 id="iti"><u>Itinéraire:</u> (cliquer sur la photo)</h3> 
 
-<a href="https://www.google.fr/maps/place/Welcom'Opale/@50.4767472,1.5958185,18z/data=!4m13!1m7!3m6!1s0x47ddda4364d63fc1:0x6593cea7d0916a29!2sAvenue+des+Peupliers,+62780+Cucq!3b1!8m2!3d50.4770894!4d1.5958303!3m4!1s0x47ddda437d8124b3:0xe0520df4fe730250!8m2!3d50.4767946!4d1.5956435"> <img id="plan" src="./images/plan.jpg" alt=""></a>
 
-<a href="https://www.google.fr/maps/@50.4946863,1.6200176,274m/data=!3m1!1e3"> Itinéraire pour arriver chez nous</a>
+<a id="bis" href="https://www.google.fr/maps/@50.4946863,1.6200176,274m/data=!3m1!1e3"> <img id="plan" src="./images/itinere.png" alt=""></a>
 
 <?php
 include './footer.php';
