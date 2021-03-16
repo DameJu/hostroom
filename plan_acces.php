88fda692131cdba4649523e3848a5ddc1202732b
Juliette <jliiette.meuniier@gmail.com>
2021-03-16 15:16:13 +0100

    adobe XD

--- plan_acces.php
+++ plan_acces.php
@@ -1,17 +1,18 @@
 <?php
 include './header.php';
 ?>
-
-<h3 class="texte" class="footer_contact"> ATTENTION ! <br> Il existe deux AVENUE DES PEUPLIERS : une sur STELLA (notre plage) et une sur TRÉPIED.
+<h3 class="texte" class="footer_contact">Retrouvez notre adresse en bas de page 
+<br><br>
+ ATTENTION ! <br> Il existe deux AVENUE DES PEUPLIERS : une sur STELLA (notre plage) et une sur TRÉPIED.
 <br>
 <br>
-Ou dans vos GPS "AVENUE LOUCHEUR", <br>  au giratoire prendre la 2ème à droite "AVENUE DU BOIS" <br> 
-et la première à gauche  "AVENUE DES PEUPLIERS<br>
+Ou mettez dans votre GPS "AVENUE LOUCHEUR", <br> puis au giratoire prendre la 2ème à droite "AVENUE DU BOIS" <br> 
+et la première à gauche  "AVENUE DES PEUPLIERS"<br>
 </h3>
 <h3 id="iti"><u>Itinéraire:</u> (cliquer sur la photo)</h3> 
 
 
-<a id="bis" href="https://www.google.fr/maps/@50.4946863,1.6200176,274m/data=!3m1!1e3"> <img id="plan" src="./images/itinere.png" alt=""></a>
+<a id="bis" target="_blank" href="https://www.google.fr/maps/@50.4946863,1.6200176,274m/data=!3m1!1e3"> <img id="plan" src="./images/itinere.png" alt=""></a>
 
 <?php
 include './footer.php';
